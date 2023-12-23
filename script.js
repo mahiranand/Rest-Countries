195ed7833d6cd6126fa7e355e6250b2a207ef0f8
mahiranand <mahiranand08@gmail.com>
2023-12-23 20:11:15 +0530

    done with dark mode and white mode

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const uploadData = async () => {
 
   const unorderList = document.querySelector(".cards");
   for (const data of datas) {
-
     const list = document.createElement("li");
     list.classList.add("card");
 
@@ -24,13 +23,15 @@ const uploadData = async () => {
     infoDiv.classList.add("info-data");
 
     const populationPara = document.createElement("p");
-    populationPara.innerHTML=`<b>Population: </b>${(data["population"]).toLocaleString()}`;
+    populationPara.innerHTML = `<b>Population: </b>${data[
+      "population"
+    ].toLocaleString()}`;
 
     const regionPara = document.createElement("p");
     regionPara.innerHTML = `<b>Region: </b>${data["region"]}`;
 
     const captialPara = document.createElement("p");
-    captialPara.innerHTML = `<b>Capital: </b>${data["capital"]}`
+    captialPara.innerHTML = `<b>Capital: </b>${data["capital"]}`;
 
     infoDiv.append(populationPara);
     infoDiv.append(regionPara);
@@ -47,3 +48,32 @@ const uploadData = async () => {
 };
 
 uploadData();
+const themeBtn = document.querySelector(".changemode");
+
+themeBtn.addEventListener("click", () => {
+  const body = document.querySelector("body");
+  if (body.style.backgroundColor == "var(--veryDarkBlue)") {
+    body.style.backgroundColor = "var(--veryLightGrey)";
+  } else {
+    body.style.backgroundColor = "var(--veryDarkBlue)";
+  }
+
+  const whiteElements = document.querySelectorAll(
+    "nav,.searchbar,.dropdown,input,.dropdown select,.card"
+  );
+  whiteElements.forEach((ele) => {
+    ele.classList.toggle("darkBackground");
+  });
+
+  const allFonts = document.querySelectorAll(
+    "nav h1, .changemode h2 , input[placeholder] , .region, .info-data p, .info-data p b, .about-data h1 "
+  );
+
+  allFonts.forEach((font) => {
+    font.classList.toggle("fontColor");
+  });
+
+  const moonIcon = document.querySelector(".changemode i");
+  moonIcon.classList.toggle("fa-regular");
+  moonIcon.classList.toggle("fa-solid");
+});
